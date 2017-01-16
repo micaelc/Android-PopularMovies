7ebe77f15dad93a69a55a74cdd27a7caa468fbc0
Micael Campos <micael.campos@gmail.com>
2017-01-16 22:17:28 +0000

    preparation of the secret.properties files

--- app/src/main/java/com/hangapps/popularmovies/MainActivity.java
+++ app/src/main/java/com/hangapps/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package com.hangapps.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TextView mHelloWorldTextView = (TextView)findViewById(R.id.tv_hello_world);
+		String tmdbApiKey = BuildConfig.TMDB_API_KEY;
+		mHelloWorldTextView.setText(tmdbApiKey);
 	}
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+##Associate Android Developer Fast Track
+###Popular Movies Project
+
+
+In order to evaluate the APP please create a secrets.properties file with your
+TMDB API KEY with the followin content
+~~~~
+TMDB_API_KEY=your_tmdb_api_key
+~~~~
